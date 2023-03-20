e244c65257e8a3cb1b7a4793966c0431adf493ca
KaranSingh1301 <70521663+KaranSingh1301@users.noreply.github.com>
2023-03-20 22:40:05 +0530

    todo-app(4)

---
+++ lec-9(Todo-4)/models/TodoModel.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const todoSchema = new Schema({
+  todo: {
+    type: String,
+    require: true,
+  },
+  username: {
+    type: String,
+    require: true,
+  },
+});
+
+module.exports = mongoose.model("todo", todoSchema);
